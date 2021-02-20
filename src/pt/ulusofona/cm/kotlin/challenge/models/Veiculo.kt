21de854aebaa96aeaa02528f61edef707f44173f
tiagofontes99 <tiagofontes99@gmail.com>
2021-02-20 17:39:11 +0000

    adicionadas algumas coisas4

--- src/pt/ulusofona/cm/kotlin/challenge/models/Veiculo.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Veiculo.kt
@@ -1,9 +1,13 @@
 package pt.ulusofona.cm.kotlin.challenge.models
 import pt.ulusofona.cm.kotlin.challenge.interfaces.Movimentavel
+import java.time.LocalDateTime
+import java.util.*
 
 
 open class Veiculo(open val identificador: String) : Movimentavel{
     var posicao : Posicao = Posicao()
+
+    var dataDeAquisicao : Date = Date()
 
     open fun requerCarta() : Boolean{
         return true
