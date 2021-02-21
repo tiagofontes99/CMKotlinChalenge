6cb841c1ecb3efa3a25b1538eaf9096384f3bb0c
tiagofontes99 <tiagofontes99@gmail.com>
2021-02-21 14:33:22 +0000

    adicionadas algumas coisas8

--- src/pt/ulusofona/cm/kotlin/challenge/models/Carro.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Carro.kt
@@ -1,7 +1,5 @@
 package pt.ulusofona.cm.kotlin.challenge.models
 
-import java.time.LocalDateTime
-import java.util.*
 
 class Carro(override val identificador: String, var motor: Motor) : Veiculo(
     identificador)
--- src/pt/ulusofona/cm/kotlin/challenge/models/Motor.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Motor.kt
@@ -5,19 +5,5 @@ import pt.ulusofona.cm.kotlin.challenge.interfaces.Ligavel
 class Motor(var cavalos: Int , var cilindrada : Int ) : Ligavel {
     var ligado : Boolean = false
 
-    override fun ligar() : Boolean{
-        ligado = true
-        return true
-    }
-
-    override fun desligar(): Boolean{
-        ligado = false
-        return false
-    }
-
-    override fun estaLigado(ligado :Boolean) : Boolean {
-        return ligado
-    }
-
 
 }
